c5cb9c253d9189fc16b2b83cb2e3467809c7d353
zhucuicui <zhucuicui@jd.com>
2021-11-09 11:56:08 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  
+</template>
+<script  lang="ts">
+ import { ref } from 'vue'
+export default {
+  data(){
+    console.log(this)
+    return {b:2}
+  },
+    setup(){ 
+      const count = ref(0);
+      let msg=111
+      console.log(this)
+      return {msg,count}
+  }
+}
+</script>
+ 
